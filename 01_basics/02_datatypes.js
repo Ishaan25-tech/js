a3fd86ec7c6dca6bceb8dd3194842acfc93d1448
Ishaan Oberoi <67230052+Ishaan25-tech@users.noreply.github.com>
2024-05-08 10:49:47 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,3 +3,22 @@
 //alert( 3 + 3)// we are using nodejs not browser
 console.log( 3 + 
     3)//code readability should be high
+console.log("Ishaan")
+
+let name = "ishaan"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint for very large numbers like in trading
+// string => ""
+// boolean => true/false
+// null => is a datatype as well as a standalone value(empty value)
+// undefined => this means it is not defined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined)// output is undefined
+console.log(typeof null)// output is object
